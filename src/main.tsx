31eab0bb15cc58453b772dd8f428e6736eeb0bcc
Rudolf Agaltsev <wardolfik@gmail.com>
2022-12-30 18:49:25 +0600

    darova

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+    <browserRouret></browserRouret>
     <App />
-  </React.StrictMode>,
+
 )
